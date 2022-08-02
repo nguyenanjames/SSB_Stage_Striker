28d2648ff8d372bb9b50d50d82db222c3baaf18e
nguyenanjames <nguyenanjames@gmail.com>
2022-08-02 12:45:34 -0700

    Initial Commit

---
+++ SSB_Stage_Striker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SSB_Stage_Striker
+//
+//  Created by Work on 8/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SSB_Stage_Striker/SSB_Stage_StrikerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSB_Stage_StrikerApp.swift
+//  SSB_Stage_Striker
+//
+//  Created by Work on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SSB_Stage_StrikerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
